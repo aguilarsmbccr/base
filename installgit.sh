df5d8b63c87aa489d93cc284eb4d1d494e50d830
aguilarsmbccr <46453652+aguilarsmbccr@users.noreply.github.com>
2019-05-27 15:54:36 -0600

    Update installgit.sh

--- installgit.sh
+++ installgit.sh
@@ -1,7 +1,14 @@
 apt-get upgrade -y
 apt-get update
+
+#git se requiere para poder clonar los repositorios correspondientes
 apt-get install -y git
-apt-get install -y ufw
+
+
+#openssh-server es para el autor una buena herramienta para acceder de manera remota al servidor mediante una consola segura.
 apt-get install -y openssh-server
+
+#ufw (Uncomplicated Fire Wall es requerido para permitir acceso a las máquinas de desarrollador a través de los puertos según sea requerido: http, https, ssh, etc.)
+apt-get install -y ufw
 ufw allow 22
 ufw enable
