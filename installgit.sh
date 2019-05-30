a76dbd471ddba05545e681a1ab7e000ac37f2299
aguilarsmbccr <46453652+aguilarsmbccr@users.noreply.github.com>
2019-05-30 12:14:30 -0600

    Update installgit.sh

--- installgit.sh
+++ installgit.sh
@@ -17,7 +17,7 @@ ufw allow 80
 ufw --force enable
 
 #All the examples will be hosted in the <em>src</em> directory in your $HOME path 
-[ -d ~/src ] || mkdir ~src
+[ -d ~/src ] || mkdir ~/src
 #; then echo "Your directory structure already contains a $HOME/src directory. Omitting its creation!";else mkdir ~/src;fi
 cd ~/src
 echo "\n\n\nYou can now use the following command to clone the DEVOPS repo:\n\ngit clone https://github.com/aguilarsmbccr/devops.git\n\n\n"
