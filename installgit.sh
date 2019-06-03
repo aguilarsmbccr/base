dfc24a448265cee72f0aa3fc3a54df45a1a13cdb
aguilarsmbccr <46453652+aguilarsmbccr@users.noreply.github.com>
2019-06-03 12:26:28 -0600

    Update installgit.sh

--- installgit.sh
+++ installgit.sh
@@ -17,20 +17,56 @@ ufw allow 80
 ufw --force enable
 
 #Here we will install Docker so that the machines are able to instantiate a Docker image easily
+
+#(From official docker website: https://docs.docker.com/install/linux/docker-ce/ubuntu/ )
+#Uninstall old versions
+apt-get remove docker docker-engine docker.io containerd runc
+
+#Setup the repository
+apt-get update
+
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+
+
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+   
+#Install Docker CE
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+
+
+
+#The following installation procedure didn't succeed
+##########################################
 #first, we need to download the dependencies
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+#apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
 #now we need to add the Docker's GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add –
 
 #install the Docker repository
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
+#add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
 
 #update the just recently installed repositories
-sudo apt-get update
+#sudo apt-get update
 
 #install the latest version of Docker
-sudo apt-get install docker-ce
+#sudo apt-get install docker-ce
+###########################################
+
+
 
 #All the examples will be hosted in the <em>src</em> directory in your $HOME path 
 [ -d ~/src ] || mkdir ~/src
